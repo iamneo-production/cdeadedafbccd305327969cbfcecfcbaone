400683582a879be85ceab7c42b2a8fa94c085901
Iamneo <techadmin@iamneo.ai>
2023-09-22 15:15:30 +0000

    Backup Commit

--- test.tf
+++ test.tf
@@ -5,9 +5,10 @@ terraform {
       }
     }
 
-    # provider "aws" {
-    #     region = "ap-northeast-1"
-    # }
+    provider "aws" {
+        region = "ap-northeast-1"
+        access_key = "ami-0ed99df77a82560e6"
+    }
 
     resource "aws_instance" "my_instance" {
         ami = "ami-0ed99df77a82560e6"
