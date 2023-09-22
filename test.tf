5a36a664984cfcecedf6262e44680e73709bb848
Iamneo <techadmin@iamneo.ai>
2023-09-22 14:56:51 +0000

    Backup Commit

--- test.tf
+++ test.tf
@@ -5,12 +5,14 @@ terraform {
       }
     }
 
-    provider "aws" {
-        region = "ap-northeast-1"
+    # provider "aws" {
+    #     region = "ap-northeast-1"
+    # }
+
+    resource "aws_instance" "my_instance" {
+        ami = "ami-0ed99df77a82560e6"
+        instance_type = "t2.micro"
+        availability_zone = "ap-northeast-1a"
     }
 
-    resource "aws_instance" "My_instance" {
-        ami = ""
-
-    }
 }
